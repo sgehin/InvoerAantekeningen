4a6163fbcee1c45e98a55b62af29836d76663273
sgehin <sfa.gehin@gmail.com>
2017-11-12 16:21:07 +0100

    pagina maken met knoppen voor sign in en sign up

--- index.php
+++ index.php
@@ -1,17 +1,59 @@
 <!DOCTYPE html>
 <!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
+Doel website maken waar je op inlog en aantekeningen kunt invoeren op je personlijke pagina.
+Deze worden opgeslagen in een database. 
+Zoek functie om aantekeningen op te halen en te displayen.
+
+Subdoel: pagina maken met knoppen voor sign in en sign up
 -->
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <link rel="shortcut icon" href="img/logo_bas.png" />
+        <link rel="stylesheet" type="text/css" href="mystyle.css">
+        <script src="myscripts.js"></script>
+        <title>myNotes</title>
+        <style>
+           
+            
+        </style>
+        <script>
+                        }
+        </script>
+        
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div>
+            <table>
+                <tr>
+                  <td >
+                    <img src="img/ogo bas.png" width="200px" Height="200px">
+                  </td>
+                  <td></td>
+                  <td>
+                      <h1 id="banner">MyNotes</h1>
+                  </td>    
+                  <?php
+                  for ($x=1;$x<4;$x++){
+                      echo "<td></td>"; 
+                  }
+                  ?>
+                  <td><form action="Login.php"><input type="submit" class="button" value="sign in"></form></td><br>
+                    <td></td><br>
+                    <td><form action="CreateAccount.php"><input type="submit" class="button" value="sign up"></form></td><br>
+                </tr>
+            </table>
+        </div>
+        <table>
+            <tr>
+                <td>
+                    <pre>
+
+                                                           
+                    </pre>
+                </td>
+            </tr>
+        </table>
+           
     </body>
 </html>
